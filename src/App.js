3c53e0b7ebf21350d7e2d61fc656f18f7460ab20
PokyCookie <0cookieboy0@gmail.com>
2022-03-27 18:53:18 +0900

    initilize Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>PORTFOLIO</p>
       </header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Category1</a>
+          </li>
+          <li>
+            <a href="#">Category2</a>
+          </li>
+          <li>
+            <a href="https://pokycookie.tistory.com/" target="_blank">
+              Tistory Blog
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <main></main>
     </div>
   );
 }
