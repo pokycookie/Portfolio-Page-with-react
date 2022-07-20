2188f992784c47cbe56026dc85d8a83d6f79900a
PokyCookie <0cookieboy0@gmail.com>
2022-07-21 03:48:49 +0900

    skills icon

--- src/components/hexagon.js
+++ src/components/hexagon.js
@@ -1,5 +1,3 @@
-import IMG_JS from "../img/js.png";
-
 export default function Hexagon(props) {
   const s = props.size;
   const w = s / (2 * Math.sqrt(3));
@@ -7,27 +5,19 @@ export default function Hexagon(props) {
   const rotateY = (deg) => {
     return {
       transform: `rotateY(${typeof deg === "number" ? deg : 0}deg)`,
-      clipPath: `path('M${w} 0 L${3 * w} 0 L${4 * w} ${s / 2} L${
-        3 * w
-      } ${s} L${w} ${s} L0 ${s / 2} Z')`,
+      clipPath: `path('M${w} 0 L${3 * w} 0 L${4 * w} ${s / 2} L${3 * w} ${s} L${w} ${s} L0 ${
+        s / 2
+      } Z')`,
+      backgroundColor: `#${props.color || "FFFFFF"}`,
+      width: 4 * w,
+      height: s,
     };
   };
 
   return (
     <div className="hexagon" style={{ margin: 0.288 * s + 10 }}>
-      <img
-        src={IMG_JS}
-        alt="js"
-        width={4 * w}
-        height={s}
-        style={
-          props.scroll > 2000 && props.scroll < 3000
-            ? rotateY(100 - (props.scroll - 2500) * 0.1)
-            : props.scroll < 2000
-            ? rotateY(180)
-            : rotateY(0)
-        }
-      />
+      <div style={props.scroll > props.init ? rotateY(0) : rotateY(180)}></div>
+      <img src={props.src} alt="js" style={props.scroll > props.init ? rotateY(0) : rotateY(180)} />
     </div>
   );
 }
--- src/pages/introduce.js
+++ src/pages/introduce.js
@@ -3,6 +3,15 @@ import Column from "../components/column";
 import ColumnNav from "../components/columnNav";
 import Hexagon from "../components/hexagon";
 
+import IMG_HTML from "../img/html.png";
+import IMG_CSS from "../img/css.png";
+import IMG_JS from "../img/js.png";
+import IMG_REACT from "../img/react.png";
+import IMG_NODE from "../img/node.png";
+import IMG_GIT from "../img/git.png";
+import IMG_TS from "../img/ts.png";
+import IMG_MONGODB from "../img/mongodb.png";
+
 const SCROLL_LEN = 2000;
 const COLUMN_LEN = 5;
 
@@ -43,27 +52,72 @@ export default function Introduce() {
   }, []);
 
   return (
-    <div
-      className="introducePage"
-      style={{ width: size.x, height: SCROLL_LEN * (COLUMN_LEN + 1) }}
-    >
+    <div className="introducePage" style={{ width: size.x, height: SCROLL_LEN * (COLUMN_LEN + 1) }}>
       <Column size={size} floor={floor} count={1} className="about">
         <p>About Me</p>
       </Column>
       <Column size={size} floor={floor} count={2} className="skills">
         <p>Skills</p>
         <div className="iconArea">
-          <Hexagon size={200} scroll={scroll} />
-          <Hexagon size={200} scroll={scroll} />
-          <Hexagon size={200} scroll={scroll} />
+          <Hexagon
+            size={200}
+            scroll={scroll}
+            src={IMG_HTML}
+            init={SCROLL_LEN * 1 + 600}
+            color="E65100"
+          />
+          <Hexagon
+            size={200}
+            scroll={scroll}
+            src={IMG_JS}
+            init={SCROLL_LEN * 1 + 800}
+            color="FFD600"
+          />
+          <Hexagon
+            size={200}
+            scroll={scroll}
+            src={IMG_NODE}
+            init={SCROLL_LEN * 1 + 1000}
+            color="43853D"
+          />
           <div className="bottom" style={{ top: 105 }}>
-            <Hexagon size={200} scroll={scroll} />
-            <Hexagon size={200} scroll={scroll} />
+            <Hexagon
+              size={200}
+              scroll={scroll}
+              src={IMG_CSS}
+              init={SCROLL_LEN * 1 + 700}
+              color="0277BD"
+            />
+            <Hexagon
+              size={200}
+              scroll={scroll}
+              src={IMG_REACT}
+              init={SCROLL_LEN * 1 + 900}
+              color="20232A"
+            />
           </div>
           <div className="bottom" style={{ top: 210 }}>
-            <Hexagon size={200} scroll={scroll} />
-            <Hexagon size={200} scroll={scroll} />
-            <Hexagon size={200} scroll={scroll} />
+            <Hexagon
+              size={200}
+              scroll={scroll}
+              src={IMG_MONGODB}
+              init={SCROLL_LEN * 1 + 1100}
+              color="023430"
+            />
+            <Hexagon
+              size={200}
+              scroll={scroll}
+              src={IMG_GIT}
+              init={SCROLL_LEN * 1 + 1200}
+              color="EAE9E1"
+            />
+            <Hexagon
+              size={200}
+              scroll={scroll}
+              src={IMG_TS}
+              init={SCROLL_LEN * 1 + 1300}
+              color="1976D2"
+            />
           </div>
         </div>
       </Column>
@@ -76,12 +130,7 @@ export default function Introduce() {
       <Column size={size} floor={floor} count={5} className="">
         5
       </Column>
-      <ColumnNav
-        count={COLUMN_LEN}
-        size={size}
-        floor={floor}
-        moveScroll={moveScroll}
-      />
+      <ColumnNav count={COLUMN_LEN} size={size} floor={floor} moveScroll={moveScroll} />
     </div>
   );
 }
